c10b6711c7c69ea36f0d35c1c7abac919c6fb65c
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-09-04 16:45:53 -0500

    style: refactor style en layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Logo from "@/components/logo";
-import DarkTheme from "@/components/darkTheme";
+import DarkTheme from "@/components/buttons/darkTheme";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} bg-F9FAFB`}>
-        <header className="flex w-full justify-between items-center py-4 px-[72px] border-b border-b-E5E7EB">
+        <header className="absolute flex w-full justify-between items-center py-4 xl:px-[72px] px-8 border-b border-b-E5E7EB">
           <Logo />
           <button className="bg-FFFFFF p-2 rounded-xl border border-E5E7EB">
             <DarkTheme />
