ae3f3d070256047f4c871c6b4183eceefd3cfa04
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-09-04 16:45:04 -0500

    feat: add redirection in logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,13 +1,16 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Logo() {
     return (
-        <Image
-            src="/logo.svg"
-            alt="logo"
-            width={120}
-            height={26}
-            priority
-        />
+        <Link href="/">
+            <Image
+                src="/logo.svg"
+                alt="logo"
+                width={120}
+                height={26}
+                priority
+            />
+        </Link>
     )
 }
