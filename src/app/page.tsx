5b58c04e67865875e6f0df78c2f16ebee2c1cb1c
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-09-05 11:53:10 -0500

    style: add font style to dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Loader from "@/components/loader";
 export default function Home() {
   return (
     <main>
-      <section className="w-full h-screen flex justify-center items-center">
+      <section className="w-full h-screen flex justify-center items-center dark:text-F9FAFBCC">
         <DropArea />
         {/* <Loader /> */}
       </section>
--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,10 +1,10 @@
 export default function Loader() {
     return (
-        <div className="flex flex-col gap-4 bg-FFFFFF py-8 px-20 max-w-[480px] w-full rounded-lg shadow-lg">
-            <p className="text-[12px] text-center font-light">
+        <div className="flex flex-col gap-4 dark:bg-212936 bg-FFFFFF py-8 px-20 max-w-[480px] w-full rounded-lg shadow-lg">
+            <p className="text-[12px] text-center font-light dark:text-F9FAFBCC">
                 <span className="font-semibold">Uploading</span>, please wait..
             </p>
-            <div className="bg-E5E7EB h-[6px] rounded-full w-full overflow-hidden relative">
+            <div className="bg-E5E7EB dark:bg-4D5562 h-[6px] rounded-full w-full overflow-hidden relative">
                 <div className="absolute bg-3662E3 w-14 h-full rounded-full animate-loader"></div>
             </div>
         </div>
