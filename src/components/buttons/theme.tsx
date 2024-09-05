f38cabfb60798ad4880c6738ca0b7854dc617966
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-09-05 18:18:16 -0500

    style: refactor styles

--- src/components/buttons/theme.tsx
+++ src/components/buttons/theme.tsx
@@ -10,7 +10,7 @@ export default function Theme() {
             className="bg-FFFFFF p-2 rounded-xl border border-E5E7EB dark:bg-4D5562/80 dark:border-4D5562">
             <input id="theme" type="checkbox" className="hidden peer" onChange={toggleTheme} />
             <Image
-                className="block peer-checked:hidden"
+                className="block peer-checked:hidden select-none"
                 src="/Moon_fill.svg"
                 alt="Dark mode"
                 height={24}
@@ -18,7 +18,7 @@ export default function Theme() {
                 priority
             />
             <Image
-                className="hidden peer-checked:block"
+                className="hidden peer-checked:block select-none"
                 src={"/Sun_fill.svg"}
                 alt="Light mode"
                 height={24}
