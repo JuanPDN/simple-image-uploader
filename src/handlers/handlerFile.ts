9e7e8f5bbb41493203de474e96bd09e59376ab35
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-09-06 13:08:39 -0500

    feat: create function to read files

--- src/handlers/handlerFile.ts
+++ src/handlers/handlerFile.ts
@@ -1,21 +1,38 @@
 const handlerFile = (event: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFiles = event.target.files;
+    selectedFiles && fileReader(selectedFiles[0]);
+};
+
+const fileReader = (file: File | null): string | null => {
+    if (!file) {
+        return null;
+    }
+
     const maxFileSize = 2 * 1024 * 1024;
 
-    if (selectedFiles) {
-        if (selectedFiles[0].size > maxFileSize) {
-            alert("Max file size is 2MB");
-            return;
-        }
+    if (file.size > maxFileSize) {
+        alert("Max file size is 2MB");
+        return null;
+    } else if (!file.type.includes("image")) {
+        alert("Please select an image file");
+        return null;
     }
 
     const fileReader = new FileReader();
-    fileReader.readAsDataURL(selectedFiles![0]);
 
-    fileReader.onload = () => {
-        const fileDataUrl = fileReader.result;
-        console.log(fileDataUrl);
-    };
-};
-
-export default handlerFile
+    try {
+        fileReader.readAsDataURL(file);
+        fileReader.onload = () => {
+            const fileDataUrl = fileReader.result as string;
+            return fileDataUrl;
+        };
+    } catch (error) {
+        console.error("Error reading file:", error);
+        return null;
+    }
+    return null;
+}
+export {
+    handlerFile,
+    fileReader
+}
