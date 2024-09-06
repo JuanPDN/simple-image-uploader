fdf294479f45d28035653764ca61dad80bde708b
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-09-06 13:07:57 -0500

    feat: add condicional loading in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
+'use client';
+
 import DropArea from "@/components/dropArea";
 import Loader from "@/components/loader";
+import { useState } from "react";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+
+
   return (
     <main>
       <section className="w-full h-screen flex justify-center items-center dark:text-F9FAFBCC">
-        <DropArea />
-        {/* <Loader /> */}
+        {!isLoading ? <DropArea /> : <Loader />}
       </section>
     </main>
   );
