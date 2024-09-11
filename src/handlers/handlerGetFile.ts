797db8d2a761af076a7971cb8183111c67d859f8
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-09-11 15:01:09 -0500

    feat: add hadlerFetFile to get file of the db

---
+++ src/handlers/handlerGetFile.ts
@@ -0,0 +1,7 @@
+import FileInterface from "@/interfaces/fileInterface";
+
+export const handlerGetFile = async (id: number): Promise<FileInterface | null> => {
+    const response = await fetch(`http://localhost:3001/upload/${id}`);
+    const data: FileInterface = await response.json();
+    return data
+}
