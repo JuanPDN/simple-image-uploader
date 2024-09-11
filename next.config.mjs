bfd1994460d0dc7fc7a058b14096f472524c4c6c
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-09-11 15:02:30 -0500

    chore: configure external images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        port: "3001",
+        hostname: "localhost",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
