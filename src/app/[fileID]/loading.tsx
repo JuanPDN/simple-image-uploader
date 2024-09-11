018353aaf7fff5dbdd5e4b2c893baa64a9fa04d4
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-09-11 15:00:21 -0500

    feat: create loading skeleton

---
+++ src/app/[fileID]/loading.tsx
@@ -0,0 +1,11 @@
+export default function Loading() {
+
+    return (
+        <div className="bg-FFFFFF dark:bg-212936 max-w-[540px] w-full p-2 rounded-xl shadow-lg mx-2 animate-pulse">
+            <div
+                className="border border-E5E7EB dark:border-4D5562 border-dashed rounded-lg h-80"
+            >
+            </div>
+        </div >
+    )
+}
