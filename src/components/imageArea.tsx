509de6fc2c33f94eedeaaa84a40294a74a069d58
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-09-11 16:22:36 -0500

    feat: Add image display functionality to ImageArea

--- src/components/imageArea.tsx
+++ src/components/imageArea.tsx
@@ -10,7 +10,7 @@ export default async function ImageArea({ id }: { id: number }) {
             <div
                 className="border border-E5E7EB dark:border-4D5562 border-dashed rounded-lg"
             >
-                {file?.name &&
+                {file?.name ?
                     <Image
                         className="rounded-lg"
                         src={`http://localhost:3001/${file.path}`}
@@ -18,7 +18,16 @@ export default async function ImageArea({ id }: { id: number }) {
                         width={524}
                         height={322}
                         priority
-                    />}
+                    /> :
+                    <Image
+                        className="rounded-lg"
+                        src={`/image_not_found.gif`}
+                        alt={"not found"}
+                        width={524}
+                        height={322}
+                        priority
+                    />
+                }
             </div>
         </div >
     )
