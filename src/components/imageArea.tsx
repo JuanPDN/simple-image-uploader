209aeceb79c27c1dfda10ac1f8b262af3542ba57
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-09-12 16:58:36 -0500

    style: remove border dashed from image area

--- src/components/imageArea.tsx
+++ src/components/imageArea.tsx
@@ -7,28 +7,24 @@ export default async function ImageArea({ id }: { id: number }) {
 
     return (
         <div className="bg-FFFFFF dark:bg-212936 max-w-[540px] w-full p-2 rounded-xl shadow-lg mx-2">
-            <div
-                className="border border-E5E7EB dark:border-4D5562 border-dashed rounded-lg"
-            >
-                {file?.name ?
-                    <Image
-                        className="rounded-lg"
-                        src={`http://localhost:3001/${file.path}`}
-                        alt={file.name}
-                        width={524}
-                        height={322}
-                        priority
-                    /> :
-                    <Image
-                        className="rounded-lg"
-                        src={`/image_not_found.gif`}
-                        alt={"not found"}
-                        width={524}
-                        height={322}
-                        priority
-                    />
-                }
-            </div>
+            {file?.name ?
+                <Image
+                    className="rounded-lg h-[322px] w-[524px]"
+                    src={`http://localhost:3001/${file.path}`}
+                    alt={file.name}
+                    width={524}
+                    height={322}
+                    priority
+                /> :
+                <Image
+                    className="rounded-lg h-[322px] w-[524px]"
+                    src={`/image_not_found.gif`}
+                    alt={"not found"}
+                    width={524}
+                    height={322}
+                    priority
+                />
+            }
         </div >
     )
 }
