b896d8bc3819fb27e1c1a3278bde1dc72220c8ca
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-09-12 16:58:01 -0500

    feat: add component download

--- src/app/[fileID]/page.tsx
+++ src/app/[fileID]/page.tsx
@@ -2,6 +2,7 @@ import ImageArea from "@/components/imageArea";
 import Loading from "@/app/[fileID]/loading";
 import { Suspense } from "react";
 import Share from "@/components/buttons/share";
+import Download from "@/components/buttons/download";
 
 export default function Home({ params }: { params: { fileID: string } }) {
 
@@ -13,8 +14,9 @@ export default function Home({ params }: { params: { fileID: string } }) {
         <Suspense fallback={<Loading />}>
           <ImageArea id={id} />
         </Suspense>
-        <div>
+        <div className="flex gap-2">
           <Share />
+          <Download id={id} />
         </div>
       </section>
     </main>
