29bcb821de838ca36b03dc1e2e5c43060ba30892
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-09-12 12:26:38 -0500

    feat: add component button share

--- src/app/[fileID]/page.tsx
+++ src/app/[fileID]/page.tsx
@@ -1,6 +1,7 @@
 import ImageArea from "@/components/imageArea";
 import Loading from "@/app/[fileID]/loading";
 import { Suspense } from "react";
+import Share from "@/components/buttons/share";
 
 export default function Home({ params }: { params: { fileID: string } }) {
 
@@ -8,10 +9,13 @@ export default function Home({ params }: { params: { fileID: string } }) {
 
   return (
     <main>
-      <section className="w-full h-screen flex justify-center items-center dark:text-F9FAFBCC">
+      <section className="w-full h-screen flex justify-center items-center dark:text-F9FAFBCC flex-col gap-6">
         <Suspense fallback={<Loading />}>
           <ImageArea id={id} />
         </Suspense>
+        <div>
+          <Share />
+        </div>
       </section>
     </main>
   );
