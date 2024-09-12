b839c2034aef5ac750e2eb8a4859437318c402e6
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-09-12 12:29:16 -0500

    feat: implement copyLink functionality for share button

---
+++ src/handlers/handlerCopy.ts
@@ -0,0 +1,14 @@
+export const handleCopyLink = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+    const url = window.location.href;
+    const button = event.currentTarget;
+    const initialText = button.outerHTML;
+
+    navigator.clipboard.writeText(url);
+    button.textContent = "Copied!";
+
+    setTimeout(() => {
+        button.outerHTML = initialText;
+    }, 2500);
+};
