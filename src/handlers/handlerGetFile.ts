a0a776da93d3441c52af9c3a5e35429e50d71336
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-09-17 08:19:37 -0500

    refactor: add method get to fecth server

--- src/handlers/handlerGetFile.ts
+++ src/handlers/handlerGetFile.ts
@@ -1,7 +1,9 @@
 import FileInterface from "@/interfaces/fileInterface";
 
 export const handlerGetFile = async (id: number): Promise<FileInterface | null> => {
-    const response = await fetch(`http://localhost:3001/upload/${id}`);
+    const response = await
+        fetch(`http://localhost:3001/upload/${id}`,
+            { method: "GET" });
     const data: FileInterface = await response.json();
     return data
 }
