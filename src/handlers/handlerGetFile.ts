0ded6ec4de05ffe59c9be058fed67ae29fa30723
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-09-17 08:51:59 -0500

    refactor: remove bugs and null pointers

--- src/handlers/handlerGetFile.ts
+++ src/handlers/handlerGetFile.ts
@@ -1,9 +1,18 @@
 import FileInterface from "@/interfaces/fileInterface";
 
 export const handlerGetFile = async (id: number): Promise<FileInterface | null> => {
-    const response = await
-        fetch(`http://localhost:3001/upload/${id}`,
-            { method: "GET" });
-    const data: FileInterface = await response.json();
-    return data
+    try {
+        const response = await fetch(`http://localhost:3001/upload/${id}`, { method: "GET" });
+        if (!response.ok) {
+            throw new Error(`Failed to get file with id ${id}. Status code: ${response.status}`);
+        }
+        const data: FileInterface | null = await response.json();
+        if (!data) {
+            throw new Error(`Failed to get file with id ${id}. Response is null`);
+        }
+        return data;
+    } catch (error) {
+        console.error("Error:", error);
+        return null;
+    }
 }
